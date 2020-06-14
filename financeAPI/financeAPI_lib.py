f6772d4d25588deb2765bb8fd8c33c76a75a6263
StengerP <PStenger@hilscher.com>
2020-06-14 22:39:54 +0200

    ##### - initial commit

---
+++ financeAPI/financeAPI_lib.py
@@ -0,0 +1,419 @@
+"""
+A finance API class using the API from https://financialmodelingprep.com/
+NOTE: You need to register on their website to obtain your own secret API key
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
+import plotly.graph_objects as go
+from urllib.request import urlopen
+
+class FinanceAPI():
+    """
+    Core class
+    """
+    
+    def __init__(self):
+        """
+        Initiates the object
+        """
+        self.key = ''
+        self.key_registered = False
+        self.originUrl = "https://financialmodelingprep.com/api/v3/"
+        pass
+    
+    def registerKey_(self,key):
+        """
+        Registers a secret API key to the object
+        """
+        assert type(key) is str,"Key must be a string"
+        self.key = str(key)
+        self.key_registered = True
+    
+    def __repr__(self):
+        return ("This is a finance API class.\n")
+    
+    def profile_data_(self,symbol):
+        """
+        Pulls the metrics data from the API for the given ticker symbol
+
+        Parameters
+        ----------
+        symbol : A ticker symbol (str) e.g. 'MSFT','FB','AAPL', or 'TWTR'
+
+        Returns
+        -------
+        None. Updates the self.profile with the data. 
+        """        
+        if not self.key_registered:
+            print("API key not registered yet.")
+            return None
+        
+        url = "https://financialmodelingprep.com/api/v3/"+\
+            "company/profile/"+str(symbol)+'?apikey='+self.key
+        response = urlopen(url)
+        data = response.read().decode("utf-8")
+        self.profile = json.loads(data)
+
+        
+    def metrics_data_(self,symbol):
+        """
+        Pulls the metrics data from the API for the given ticker symbol
+
+        Parameters
+        ----------
+        symbol : A ticker symbol (str) e.g. 'MSFT','FB','AAPL', or 'TWTR'
+
+        Returns
+        -------
+        None. Updates the self.metrics with the data. 
+        """
+        if not self.key_registered:
+            print("API key not registered yet.")
+            return None
+        
+        url = "https://financialmodelingprep.com/api/v3/"+\
+            "company-key-metrics/"+str(symbol)+'?apikey='+self.key
+        response = urlopen(url)
+        data = response.read().decode("utf-8")
+        self.metrics = json.loads(data)
+        
+    def ratios_data_(self,symbol):
+        """
+        Pulls the ratios data from the API for the given ticker symbol
+
+        Parameters
+        ----------
+        symbol : A ticker symbol (str) e.g. 'MSFT','FB','AAPL', or 'TWTR'
+
+        Returns
+        -------
+        None. Updates the self.ratios with the data. 
+        """
+        if not self.key_registered:
+            print("API key not registered yet.")
+            return None
+        
+        url = "https://financialmodelingprep.com/api/v3/"+\
+            "ratios/"+str(symbol)+'?apikey='+self.key
+        response = urlopen(url)
+        data = response.read().decode("utf-8")
+        self.ratios = json.loads(data)
+
+    def income_statement_data_(self, symbol):
+        """
+        Pulls the income statement data from the API for the given ticker symbol
+
+        Parameters
+        ----------
+        symbol : A ticker symbol (str) e.g. 'MSFT','FB','AAPL', or 'TWTR'
+
+        Returns
+        -------
+        None. Updates the self.metrics with the data.
+        """
+        if not self.key_registered:
+            print("API key not registered yet.")
+            return None
+
+        url = self.originUrl+"income-statement/"+str(symbol)+"?apikey="+self.key
+        response = urlopen(url)
+        data = response.read().decode("utf-8")
+        self.income_statement = json.loads(data)
+
+    
+    def build_dict(self,symbol,save=False):
+        """
+        Builds a dictionary with a given ticker symbols
+        
+        Parameters
+        ----------
+        symbol : A ticker symbol (str) e.g. 'MSFT','FB','AAPL', or 'TWTR'
+
+        Returns
+        -------
+        A dictionary with all the profile and metrics data pulled from the API
+        """
+        # Pull data
+        if not self.key_registered:
+            print("API key not registered yet.")
+            return None
+        
+        self.profile_data_(symbol)
+        # self.metrics_data_(symbol)
+        # self.ratios_data_(symbol)
+        self.income_statement_data_(symbol)
+        # Empty dict
+        data_dict = {}
+        # Symbol
+        data_dict['symbol'] = symbol
+        # Profile data
+        for k in self.profile['profile'].keys():
+            data_dict[k] = self.profile['profile'][k]
+        # # Metrics data
+        # for k in self.metrics['metrics'][0].keys():
+        #     data_dict[k]=self.metrics['metrics'][0][k]
+        # # Ratios data
+        # c=[(k,v) for k,v in self.ratios[0].items()]
+        # for k in c[2:]:
+        #     data_dict[k[0]]=k[1]
+
+        data_dict["income_statement"] = {}
+        for num in range(0, len(self.income_statement)):
+            dict_incomeStatement = {}
+            for k in self.income_statement[num].keys():
+                dict_incomeStatement[k] = self.income_statement[num][k]
+            # just use year for indexing the single statements -> [0:4]
+            data_dict["income_statement"][self.income_statement[num]["date"][0:4]] = dict_incomeStatement
+
+        if save:
+            with open("data/" + symbol + ".json", 'w') as file:
+                # Serialize data into file:
+                json.dump(data_dict, file)
+        return data_dict
+
+
+    def available_data(self,data_type='profile'):
+        """
+        Prints a list of data items that are available
+        
+        Arguments
+        ----------
+        data_type: One of 'profile','metrics',or 'ratios'
+        
+        Returns
+        --------
+        None. Prints a list.
+        """
+        assert str(data_type) in ['profile','metrics','ratios'], \
+        "Data type not recognized. Should be one of \'profile\',\'metrics\',or \'ratios\'"
+        
+        available_data = []
+        if data_type=='profile':    
+            for k in self.profile['profile'].keys():
+                if self.profile['profile'][k] is not None:
+                    available_data.append(k)
+            print("Following data are available for profile")
+            print("="*60)
+            for d in available_data[:-1]:
+                print(d,end=', ')
+            print(available_data[-1],end='.')
+        
+        if data_type=='metrics':    
+            for k in self.metrics['metrics'][0].keys():
+                if self.metrics['metrics'][0][k] is not None:
+                    available_data.append(k)
+            print("Following data are available for metrics")
+            print("="*60)
+            for d in available_data[:-1]:
+                print(d,end=', ')
+            print(available_data[-1],end='.')
+            
+        if data_type=='ratios':    
+            c=[(k,v) for k,v in self.ratios[0].items()]
+            for k in c[2:]:
+                if k[1] is not None:
+                    available_data.append(k[0])
+            print("Following data are available for ratios")
+            print("="*60)
+            for d in available_data[:-1]:
+                print(d,end=', ')
+            print(available_data[-1],end='.')
+    
+    def cols_numeric_(self):
+        """
+        Transforms columns to numeric (float) wherever applicable
+        """
+        for c in self.df.columns:
+            try:
+                self.df[c] = self.df[c].apply(float)
+            except ValueError:
+                pass
+    
+    def replace_None_(self):
+        """
+        Replaces NoneType data by np.nan in the DataFrame
+        """
+        self.df.fillna(value=pd.np.nan, inplace=True)
+    
+    def build_dataframe(self,lst):
+        """
+        Builds a DataFrame with a given list of ticker symbols
+        
+        Parameters
+        ----------
+        lst : A list of ticker symbols (str) 
+            e.g. ['MSFT','FB','AAPL','TWTR']
+
+        Returns
+        -------
+        A Pandas DataFrame with all the data pulled from the API, 
+        indexed by the symbol (company)
+        """
+        if not self.key_registered:
+            print("API key not registered yet.")
+            return None
+        
+        data_companies = []
+        for c in lst:
+            data_companies.append(self.build_dict(c, save=True))
+        # Build the DataFrame
+        self.df = pd.DataFrame(data_companies)
+        # Convert to numeric columns wherever applicable
+        self.replace_None_()
+        self.cols_numeric_()
+        
+        return self.df
+    
+    def bar_chart(self,var='price',**kwargs):
+        """
+        Plots a bar chart of the given variable
+        
+        Parameters
+        ----------
+        var : Name of the variable to be plotted (str)
+            e.g. 'price', 'PE ratio', 'Book Value per Share'
+
+        Returns
+        -------
+        None
+        """
+        plt.figure(figsize=(10,4))
+        plt.title("{}".format(var),fontsize=18)
+        plt.bar(x=self.df['companyName'],height=self.df[var],**kwargs)
+        plt.xticks(fontsize=14,rotation=45)
+        plt.yticks(fontsize=14)
+        plt.ylabel(var,fontsize=16)
+        plt.show()
+
+    def scatter(self,varX,varY,sizeZ=None,**kwargs):
+        """
+        Plots a scatter chart of the given variable pair,
+        and (optionally) size the as per a third variable
+        
+        Parameters
+        ----------
+        varX : Name of the x-axis variable (str), e.g. 'price', 'PE ratio', 'Book Value per Share'
+        varY : Name of the x-axis variable (str), e.g. 'price', 'PE ratio', 'Book Value per Share'
+        sizeZ : Name of the variable (str) for sizing, e.g. 'price', 'PE ratio', 'Book Value per Share'
+
+        Returns
+        -------
+        None
+        """
+        if sizeZ is not None:
+            scale = 1000/(self.df[sizeZ].max()**2)
+        plt.figure(figsize=(10,4))
+        plt.title("{} vs. {}".format(varX,varY),fontsize=18)
+        if sizeZ is not None:
+            plt.scatter(x=self.df[varX],y=self.df[varY],s=scale*self.df[sizeZ]**2,**kwargs)
+        else:
+            plt.scatter(x=self.df[varX],y=self.df[varY],s=200,**kwargs)
+        for i in (range(len(self.df))):
+            plt.annotate(s=self.df['symbol'][i],
+                         xy=(self.df[varX][i],
+                             self.df[varY][i]))
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+        plt.xlabel(varX,fontsize=16)
+        plt.ylabel(varY,fontsize=16)
+        plt.show()
+
+    def chart(self, input,type, **kwargs):
+        """
+        Plots a bar chart of the given input[type]
+
+        Parameters
+        ----------
+        var : Name of the variable to be plotted (str)
+            e.g. 'price', 'PE ratio', 'Book Value per Share'
+
+        Returns
+        -------
+        None
+        """
+        data_x = []
+        data_y = []
+        for k in input.keys():
+            data_x.append(k)
+            data_y.append(input[k][type])
+        #growth = (data_y[1] - data_y[0])/data_y[1]
+        fig = go.Figure(data=[go.Bar(x=data_x,y=data_y)]) #hovertext=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19'])])
+        # Customize aspect
+        fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)',
+                          marker_line_width=1.5, opacity=0.6)
+        fig.update_layout(title_text='Historical '+type, yaxis=dict(title="Revenue in Billion USD"), xaxis=dict(title="year"))
+        fig.show()
+
+    def start_stock_screener(self):
+        parameters = self.select_filter()
+
+        results = self.callAPI_financialModelingPrep_(parameters, "stockscreener")
+
+        for e in range(0, len(results)):
+            print(results[e]["symbol"])
+
+
+    def select_filter(self):
+        """
+        set filter Parameters for the stock screener search
+
+        Parameters
+        ----------
+        available Parameters:   marketCapMoreThan & marketCapLowerThan : number  (zB. marketCapMoreThan=1000000)
+                                betaMoreThan & betaLowerThan           : number
+                                volumeMoreThan & volumeLowerThan       : number
+                                dividendMoreThan & dividendLowerThan   : number
+                                sector : Consumer Cyclical - Energy - Technology - Industrials - Financial Services
+                                         Basic Materials - Communication Services - Consumer Defensive - Healthcare
+                                         Real Estate - Utilities - Industrial Goods - Financial - Services - Conglomerates
+                                exchange : nyse - nasdaq - amex - euronex - tsx - etf - mutual_fund
+                                limit : number (limit of results)
+
+        example : https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&volumeMoreThan=10000&sector=Technology&exchange=NASDAQ&dividendMoreThan=0&limit=100&apikey=key
+
+        Returns
+        -------
+
+        dict with filter Parameters
+        """
+        # place read of a GUI here later where Parameters can be selected.
+        # for now static parameters:
+        parameters = {"marketCapMoreThan": 100000000000, "sector": "Healthcare", "limit": 10}
+
+        return parameters
+
+    def callAPI_financialModelingPrep_(self, input_data, call="profile"):
+        """
+        make call of the financialModelingPrep API with given parameters
+
+        Parameters
+        ----------
+        input_data : dict with parameters for the call (see https://financialmodelingprep.com/developer/docs/#Stock-Screener)
+        call : specifiy the type of API call for constructing the string
+
+        Returns
+        -------
+        results as a dictionary
+        """
+        if not self.key_registered:
+            print("API key not registered yet.")
+            return None
+
+        if call is 'stockscreener':
+            # TODO : data auf dict prüfen bzw richtige Eingabeparameter
+            parameters = ""
+            for k in input_data.keys():
+                parameters = parameters+k+"="+str(input_data[k])+"&"
+            url = "https://financialmodelingprep.com/api/v3/stock-screener?" + parameters + 'apikey=' + self.key
+            response = urlopen(url)
+            return json.loads(response.read().decode("utf-8"))
+        else:
+            print("Type of API call not supported: "+call)
+
+        # TODO support other types of API calls in this function
+        #if call is "profile":
+        #if call is "metrics":
+        #if call is "ratios":
---
+++ main.py
@@ -0,0 +1,78 @@
+# Main script
+from financeAPI.financeAPI_lib import FinanceAPI
+# Other libraries
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
+import plotly.express as px
+from urllib.request import urlopen
+
+name_testdict = "BABA"
+
+
+f = FinanceAPI()
+print(f)
+
+# Check for API Key and load
+if Path("financeAPI/Secret_Key.txt").is_file():
+    with open('financeAPI/Secret_Key.txt') as file:
+        key = file.read()
+    f.registerKey_(key)
+else:
+    print("No file with API key found")
+    exit()
+
+f.start_stock_screener()
+
+# Read data from file if it exists:
+if Path("data/"+name_testdict+".json").is_file():
+    with open("data/"+name_testdict+".json") as file:
+        apple_dict = json.load(file)
+else:
+    apple_dict = f.build_dict('AAPL')
+    with open("data/"+name_testdict+".json",'w') as file:
+        # Serialize data into file:
+        json.dump(apple_dict, file)
+
+f.chart(apple_dict["income_statement"], type="revenue")
+
+#apple_dict=f.build_dict('AAPL')
+
+#
+# for k,v in apple_dict.items():
+#     print("{}: {}".format(k,v))
+#
+# df = f.build_dataframe(['FB','MSFT','AAPL',"AMZN","NFLX", "GOOG"])
+#
+# f.available_data('profile')
+#
+# f.available_data('metrics')
+#
+# f.available_data('ratios')
+#
+# f.bar_chart('Book Value per Share',color='orange',edgecolor='k')
+#
+# f.bar_chart('debtEquityRatio')
+#
+# f.scatter('quickRatio','Book Value per Share',color='blue')
+#
+# f.scatter(varX='debtEquityRatio',
+#           varY='Enterprise Value over EBITDA',
+#          sizeZ='price',
+#          color='red',alpha=0.6)
+#
+# # Only companies with market cap > 200 billion USD
+# df = f.df
+# df_large_cap = df[df['Market Cap']>200e9]
+# df_large_cap[['companyName','Market Cap']]
+#
+#
+# # A fresh class declration
+# f2 = FinanceAPI()
+# # Assigning the custom DataFrame to the `df` attribute of this new class object
+# # Note we did not need to request data from the API again.
+# f2.df = df_large_cap
+#
+# f2.bar_chart('Enterprise Value over EBITDA',color='red',edgecolor='k')
