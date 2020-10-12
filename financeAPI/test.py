e38702dfad23ee3b0242c82031d23f6ea9b42404
StengerP <PStenger@hilscher.com>
2020-10-13 00:26:17 +0200

    ##### - Test commit

---
+++ financeAPI/test.py
@@ -0,0 +1,22 @@
+"""
+Brainstorming
+"""
+###################################
+"""
+Bausteine:
+"""
+# Aktienanalysen & Kennzahlen
+# Visualisierung & Historie
+# Vergleich von Unternehmen
+# Fundamentaldaten
+# Selektion mit Filterkriterien (GUI)
+# Portfolio Manager
+# Datenbanken
+# KI Support
+# Newsfeeds
+
+"""
+Functions
+"""
+#
+print("hey")
